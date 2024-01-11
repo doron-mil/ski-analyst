ecb62cdd4b92be1c38bdc7f951ef5b3608f8c493
Doron Miller <doron.miller@gmail.com>
2024-01-11 20:58:28 +0100

    Step #5 in displaying the Graph : Improving the display especially in Lon/Lat

--- src/dialogs/GraphDialog.vue
+++ src/dialogs/GraphDialog.vue
@@ -4,9 +4,11 @@
 
       <div>
         <apexchart ref="realtimeChart" width="100%" height="90%" type="line" :options="options"
-                   :series="series"></apexchart>
+                   :series="dataSeries"></apexchart>
       </div>
+      <div v-html="showResults">
 
+      </div>
       <button @click="closeDialog()">
         Close
       </button>
@@ -22,6 +24,7 @@ import {useStore} from 'vuex';
 import {getGpxData} from '../store/ski-data/getters.ts';
 import moment from 'moment';
 import {GpxOnePosRecord} from '../model/GpxOnePosRecord.ts';
+import {GpxRecordInterface} from '../model/types.ts';
 
 export default {
   name: "GraphDialog",
@@ -29,7 +32,9 @@ export default {
   setup() {
     const store = useStore();
 
-    const graphData: { x: number, y: number, y2: number }[] = ref();
+    const graphData: GpxRecordInterface[] = ref();
+
+    const showResults = ref();
 
     const options = ref({
       chart: {
@@ -46,6 +51,20 @@ export default {
       },
 
       tooltip: {
+        y: {
+          formatter: function (value, {series, seriesIndex, dataPointIndex, w}) {
+            let newValue = value;
+            if (series[seriesIndex].length) {
+              const seriesName = dataSeries.value[seriesIndex].name;
+              if (['lat', 'lon'].includes(seriesName)) {
+                newValue = graphData.value[dataPointIndex][seriesName];
+              }
+
+              // console.log('eeee', w.config.series[seriesIndex].name,dataSeries.value[seriesIndex].name);
+            }
+            return newValue;
+          }
+        },
         custom: function ({series, seriesIndex, dataPointIndex, w}) {
           // let title = w.globals.tooltip.tooltipTitle.outerHTML;
           const titleOuterText = Number.parseInt(w.globals.tooltip.tooltipTitle.outerText);
@@ -56,30 +75,34 @@ export default {
               + `</div>`;
 
 
-        // console.log('wwwww', w.globals.tooltip);
+          // console.log('wwwww', w.globals.tooltip);
+          // Working
           let items = "";
           w.globals.tooltip.ttItems.forEach(x => {
             // const doc = new DOMParser().parseFromString(ht, "text/xml");
             // const doc = new DOMParser(x);
             // console.log('wwwww','\n\n', doc.documentElement.outerHTML,'\n\n',x.outerHTML);
-            if (!x.getAttribute('style').includes('none')) {
-              console.log('wwwww', '\n\n', x.getAttribute('style'),
-                  x.getElementsByClassName("apexcharts-tooltip-text-y-label")[0].outerText
-              );
-              if (x.getElementsByClassName("apexcharts-tooltip-text-y-label")[0].outerText.includes('lat')) {
-                if (x.getElementsByClassName("apexcharts-tooltip-text-y-value").length) {
-                  // console.log('w3', x.getElementsByClassName("apexcharts-tooltip-text-y-value")[0].innerText);
-                  x.getElementsByClassName("apexcharts-tooltip-text-y-value")[0].innerText = 'aaa';
-                } else {
-                  console.log('w2', x.outerHTML);
-                }
 
-              }
-              // items = items + ht;
-              // items = items + doc.documentElement.outerHTML;
-              items = items + x.outerHTML;
-            }
+            // if (!x.getAttribute('style').includes('none')) {
+            //   console.log('wwwww', '\n\n', x.getAttribute('style'),
+            //       x.getElementsByClassName("apexcharts-tooltip-text-y-label")[0].outerText
+            //   );
+            //   if (x.getElementsByClassName("apexcharts-tooltip-text-y-label")[0].outerText.includes('lat')) {
+            //     if (x.getElementsByClassName("apexcharts-tooltip-text-y-value").length) {
+            //       // console.log('w3', x.getElementsByClassName("apexcharts-tooltip-text-y-value")[0].innerText);
+            //       x.getElementsByClassName("apexcharts-tooltip-text-y-value")[0].innerText = 'aaa';
+            //     } else {
+            //       console.log('w2', x.outerHTML);
+            //     }
+            //   }
+
+            // items = items + ht;
+            // items = items + doc.documentElement.outerHTML;
+            // items = items + x.outerHTML;
+            // }
+            items = items + x.outerHTML;
           });
+          showResults.value = items;
           // console.log('wwwww1', items);
           // console.log('wwwww1', w.globals.tooltip);
           return title + items;
@@ -93,6 +116,7 @@ export default {
     });
 
     const yaxisTemplate = {
+      show: true,
       decimalsInFloat: 2,
       axisTicks: {
         show: true
@@ -107,14 +131,14 @@ export default {
         }
       },
       title: {
-        text: "Series A",
+        text: "",
         style: {
           color: "#FF1654"
         }
       }
     };
 
-    const series = ref(
+    const dataSeries = ref(
         []
     );
 
@@ -136,24 +160,32 @@ export default {
           });
 
         } else {
-          const newYAxisProp = Object.assign({}, yaxisTemplate);
+          const newYAxisProp = structuredClone( yaxisTemplate);
           newYAxisProp.title.text = key;
           options.value.yaxis.push(newYAxisProp);
 
           const data = graphData.value.map((record) => {
             let recordAttr = record[key] ?? 0;
-            if (key === 'lat' || key === 'lon') {
-              recordAttr = Math.trunc((recordAttr - Math.trunc(recordAttr)) * 10000);
-            } else if (key === 'dist' || key === 'distE' || key === 'speed') {
-              recordAttr = Number(recordAttr).toFixed(2);
-            } else if (key === 'dEle') {
-              recordAttr = Number(recordAttr).toFixed(1);
+            switch (key) {
+              case 'lat':
+              case 'lon':
+                recordAttr = Math.trunc((recordAttr - Math.trunc(recordAttr)) * 10000);
+                break;
+              case 'dist':
+              case 'distE':
+              case 'speed':
+                recordAttr = Number(recordAttr).toFixed(2);
+                break;
+              case 'alt':
+              case 'dEle':
+                recordAttr = Number(recordAttr).toFixed(1);
+                break;
             }
 
             return recordAttr;
           });
 
-          series.value.push(
+          dataSeries.value.push(
               {
                 name: key,
                 data
@@ -161,7 +193,7 @@ export default {
         }
       });
 
-      console.log('1111', series.value);
+      // console.log('1111', series.value);
       // graphData.value = data.slice(0, 100).map((record: GpxOnePosRecord) =>
       //     ({x: record.time, y: record.dist ??= 0, y2: record.distE ??= 0}));
       //
@@ -182,7 +214,7 @@ export default {
       // console.log('1111', series.value[0].data);
 
       nextTick(() => {
-        series.value.forEach((series) => {
+        dataSeries.value.forEach((series) => {
           if (series.name !== 'speed') {
             realtimeChart.value.hideSeries(series.name);
           }
@@ -194,8 +226,9 @@ export default {
 
     return {
       options,
-      series,
+      dataSeries,
       realtimeChart,
+      showResults,
     };
   }
 };
